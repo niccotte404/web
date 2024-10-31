f09386c9bde976f6e86339c50789f728c5259214
niccotte <niccotte404@gmail.com>
2024-10-31 14:29:33 +0300

    fix timer

--- scripts/time_loader.js
+++ scripts/time_loader.js
@@ -1,7 +1,7 @@
 (function load_time() {
     var before_loadtime = new Date().getTime();
     document.addEventListener('DOMContentLoaded', () => {
-        var loadTime = performance.now();
+        var loadTime = new Date().getTime() - performance.timing.navigationStart;
         document.getElementById("load_time").innerHTML = "Page load time is <b>" + Math.round(loadTime) + "</b> mc";
     })
 }());
